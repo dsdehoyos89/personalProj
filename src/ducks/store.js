0e0db96fd5623d21b6408ddfa99ceea9d2357eab
Daniel De Hoyos <dsdehoyos@gmail.com>
2018-12-18 13:46:12 -0600

    final touches

--- src/ducks/store.js
+++ src/ducks/store.js
@@ -1,6 +1,6 @@
-import { createStore, compose, applyMiddleware } from 'redux';
-import promiseMiddleware from 'redux-promise-middleware';
-import reducer from '../ducks/reducer';
+import { createStore, compose, applyMiddleware } from "redux";
+import promiseMiddleware from "redux-promise-middleware";
+import reducer from "../ducks/reducer";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 let storeMiddleware = composeEnhancers(applyMiddleware(promiseMiddleware()));
